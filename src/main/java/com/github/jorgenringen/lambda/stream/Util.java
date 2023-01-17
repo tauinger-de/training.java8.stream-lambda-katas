1a771d4e82306168c60c7d98b0c746c79e54a1e8
Thomas Auinger <thomas@auinger.de>
2023-01-17 12:16:57 +0100

    Fixed internal Exception usage

--- src/main/java/com/github/jorgenringen/lambda/stream/Util.java
+++ src/main/java/com/github/jorgenringen/lambda/stream/Util.java
@@ -1,7 +1,5 @@
 package com.github.jorgenringen.lambda.stream;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.IntSummaryStatistics;
 import java.util.List;
 import java.util.Map;
@@ -9,42 +7,54 @@ import java.util.Map;
 public class Util {
 
     public static List<String> mapToUppercase(List<String> input) {
-        throw new NotImplementedException();    }
+        throw new RuntimeException("not implemented");
+    }
 
     public static List<String> removeElementsWithMoreThanFourCharacters(List<String> input) {
-        throw new NotImplementedException();    }
+        throw new RuntimeException("not implemented");
+    }
 
     public static List<String> sortStrings(List<String> input) {
-        throw new NotImplementedException();    }
+        throw new RuntimeException("not implemented");
+    }
 
     public static List<Integer> sortIntegers(List<String> input) {
-        throw new NotImplementedException();    }
+        throw new RuntimeException("not implemented");
+    }
 
     public static List<Integer> sortIntegersDescending(List<String> input) {
-        throw new NotImplementedException();    }
+        throw new RuntimeException("not implemented");
+    }
 
     public static Integer sum(List<Integer> numbers) {
-        throw new NotImplementedException();    }
+        throw new RuntimeException("not implemented");
+    }
 
     public static List<String> flattenToSingleCollection(List<List<String>> input) {
-        throw new NotImplementedException();
+        throw new RuntimeException("not implemented");
     }
 
     public static String separateNamesByComma(List<Person> input) {
-        throw new NotImplementedException();    }
+        throw new RuntimeException("not implemented");
+    }
 
     public static String findNameOfOldestPerson(List<Person> input) {
-        throw new NotImplementedException();    }
+        throw new RuntimeException("not implemented");
+    }
 
     public static List<String> filterPeopleLessThan18YearsOld(List<Person> input) {
-        throw new NotImplementedException();    }
+        throw new RuntimeException("not implemented");
+    }
 
     public static IntSummaryStatistics getSummaryStatisticsForAge(List<Person> input) {
-        throw new NotImplementedException();    }
+        throw new RuntimeException("not implemented");
+    }
 
     public static Map<Boolean, List<Person>> partitionAdults(List<Person> input) {
-        throw new NotImplementedException();    }
+        throw new RuntimeException("not implemented");
+    }
 
     public static Map<String, List<Person>> partitionByNationality(List<Person> input) {
-        throw new NotImplementedException();    }
+        throw new RuntimeException("not implemented");
+    }
 }
